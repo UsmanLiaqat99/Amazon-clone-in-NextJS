816484956c5e8f9dbad405231b7cdbbb4097fd39
Usman Liaqat <chusmanliaqat1020@gmail.com>
2022-10-21 10:53:38 +0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzwSvqIYBe7z_Z3IT3juhKtL6jGBtfJaQ",
+    authDomain: "amz2-clone.firebaseapp.com",
+    projectId: "amz2-clone",
+    storageBucket: "amz2-clone.appspot.com",
+    messagingSenderId: "257877144908",
+    appId: "1:257877144908:web:3251b4265fa92986b5cbe9"
+};
